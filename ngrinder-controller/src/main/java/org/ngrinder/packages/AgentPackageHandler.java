4cb75b8071c312adf59e06dd4430f58532d0df8a
leedonggyu <donggyu.lee@nts-corp.com>
2020-10-23 12:11:38 +0900

    Use constructor based dependency injection

--- ngrinder-controller/src/main/java/org/ngrinder/packages/AgentPackageHandler.java
+++ ngrinder-controller/src/main/java/org/ngrinder/packages/AgentPackageHandler.java
@@ -1,8 +1,9 @@
 package org.ngrinder.packages;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang.StringUtils;
 import org.ngrinder.infra.schedule.ScheduledTaskService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -13,15 +14,14 @@ import java.util.Set;
 import static org.ngrinder.common.util.CollectionUtils.newHashMap;
 
 @Component("agentPackageHandler")
+@RequiredArgsConstructor
 public class AgentPackageHandler extends PackageHandler {
 
-	@Autowired
-	private ScheduledTaskService scheduledTaskService;
+	private final ScheduledTaskService scheduledTaskService;
 
 	@PostConstruct
 	public void cleanUpCachedPackageDir() {
 		cleanUpPackageDir(true);
-
 		scheduledTaskService.addFixedDelayedScheduledTask(() -> cleanUpPackageDir(false), TIME_MILLIS_OF_DAY);
 	}
 
